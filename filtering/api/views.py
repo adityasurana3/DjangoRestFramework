e16b10e6bae05d0b4a074a885b38c4f3ea2ff085
Aditya Surana <suranaaditya3@gmail.com>
2023-08-22 18:18:04 +0530

    Filtering in DRF using django-filters

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Student
 from .serializers import StudentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 class StudentAPI(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get_queryset(self):
-        return Student.objects.filter(passby=self.request.user)
+    ################################### Method 1 without using any library ###################################
+    # def get_queryset(self):
+    #     return Student.objects.filter(passby=self.request.user)
+    
+    ################################### Method 2 using django-filters ###################################
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name','city']
+
+
     
